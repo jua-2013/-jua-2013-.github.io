7615b75bf3b63d4ea58abc33b1344c40abe163ba
jua-2013 <139175175+jua-2013@users.noreply.github.com>
2024-03-05 19:59:35 -0300

    Delete js directory

--- js/historia.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function(){
-    var inicio = document.getElementById('inicio')
-    inicio.classList.add('hidden')
-    var site = document.getElementById('site')
-    site.classList.add('site2')
-}, 6000)
